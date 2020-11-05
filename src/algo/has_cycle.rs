d630c3c9e55e178617c56b7ebd6bbb78341f50a4
m.amin.rayej <m.amin.rayej@gmail.com>
2020-11-05 13:32:36 +0330

    change has_cycle to adopt new listener pattern.

--- src/algo/has_cycle.rs
+++ src/algo/has_cycle.rs
@@ -1,80 +1,80 @@
 use crate::provide;
-use crate::traversal::{Color, Dfs};
+use crate::traversal::{Color, Dfs, DfsListener};
 
 use magnitude::Magnitude;
-use std::cell::RefCell;
 
-pub struct HasCycle {
-    parents: RefCell<Vec<Magnitude<usize>>>,
-    stack: RefCell<Vec<usize>>,
+pub struct HasCycle<'a, G> {
+    parents: Vec<Magnitude<usize>>,
+    stack: Vec<usize>,
+    cycle: Vec<usize>,
+    graph: &'a G,
 }
 
-impl HasCycle {
-    pub fn init<G>(graph: &G) -> Self
-    where
-        G: provide::Neighbors + provide::Vertices,
-    {
-        HasCycle {
-            parents: RefCell::new(vec![Magnitude::PosInfinite; graph.vertex_count()]),
-            stack: RefCell::new(vec![]),
+impl<'a, G: provide::Neighbors + provide::Vertices> DfsListener for HasCycle<'a, G> {
+    fn on_start(&mut self, _: &Dfs<Self>, virt_id: usize) {
+        self.stack.push(virt_id);
+    }
+
+    fn on_white(&mut self, dfs: &Dfs<Self>, virt_id: usize) {
+        self.parents[virt_id] = (*self.stack.last().unwrap()).into();
+
+        if self.parents[virt_id] != virt_id.into() {
+            self.stack.push(virt_id);
+        }
+
+        // detect cycle
+        let real_id = dfs.get_id_map().get_virt_to_real(virt_id).unwrap();
+
+        if let Some(start_virt_id) = self
+            .graph
+            .neighbors(real_id)
+            .into_iter()
+            .map(|real_id| dfs.get_id_map().get_real_to_virt(real_id).unwrap())
+            .find(|&n_id| {
+                dfs.get_colors()[n_id] != Color::White && self.parents[virt_id] != n_id.into()
+            })
+        {
+            // trace back to vertex with start_virt_id
+            let mut parent = virt_id;
+            while parent != start_virt_id {
+                self.cycle.push(parent);
+                parent = self.parents[parent].unwrap();
+            }
+
+            self.cycle.push(parent);
+
+            self.cycle.reverse()
         }
     }
 
-    pub fn detect<G>(&self, graph: &G) -> Vec<usize>
-    where
-        G: provide::Neighbors + provide::Vertices,
-    {
-        let mut cycle = Vec::new();
+    fn on_black(&mut self, _: &Dfs<Self>, _: usize) {
+        self.stack.pop();
+    }
+}
 
-        let dfs = Dfs::init(graph);
-
-        dfs.execute(
+impl<'a, G: provide::Neighbors + provide::Vertices> HasCycle<'a, G> {
+    pub fn init(graph: &'a G) -> Self {
+        HasCycle {
+            parents: vec![Magnitude::PosInfinite; graph.vertex_count()],
+            stack: vec![],
+            cycle: vec![],
             graph,
-            |virt_id| {
-                self.stack.borrow_mut().push(virt_id);
-            },
-            |virt_id| {
-                self.parents.borrow_mut()[virt_id] = (*self.stack.borrow().last().unwrap()).into();
+        }
+    }
 
-                if self.parents.borrow()[virt_id] != virt_id.into() {
-                    self.stack.borrow_mut().push(virt_id);
-                }
+    pub fn detect(mut self, graph: &G) -> Vec<usize> {
+        let dfs = Dfs::init(graph, &mut self);
 
-                // detect cycle
-                let real_id = dfs.get_id_map().get_virt_to_real(virt_id).unwrap();
+        dfs.execute(graph);
 
-                if let Some(start_virt_id) = graph
-                    .neighbors(real_id)
-                    .into_iter()
-                    .map(|real_id| dfs.get_id_map().get_real_to_virt(real_id).unwrap())
-                    .find(|&n_id| {
-                        dfs.get_colors()[n_id] != Color::White
-                            && self.parents.borrow()[virt_id] != n_id.into()
-                    })
-                {
-                    // trace back to vertex with start_virt_id
-                    let mut parent = virt_id;
-                    while parent != start_virt_id {
-                        cycle.push(parent);
-                        parent = self.parents.borrow()[parent].unwrap();
-                    }
+        let id_map = dfs.id_map();
 
-                    cycle.push(parent);
+        let a = self.cycle
+            .iter()
+            .map(|virt_id| id_map.get_virt_to_real(*virt_id).unwrap())
+            .collect();
 
-                    cycle.reverse()
-                }
-            },
-            |_| (),
-            |_| {
-                self.stack.borrow_mut().pop();
-            },
-            || ()
-        );
-
-        cycle
-            .into_iter()
-            .map(|virt_id| dfs.get_id_map().get_virt_to_real(virt_id).unwrap())
-            .collect()
+        a
     }
 }
 
