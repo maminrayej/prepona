044de9e85f190a60548f46e03cc37a02a6dc596b
m.amin.rayej <m.amin.rayej@gmail.com>
2020-11-16 20:10:44 +0330

    add tests for id_map

--- src/provide/id_map.rs
+++ src/provide/id_map.rs
@@ -26,6 +26,74 @@ impl IdMap {
     }
 
     pub fn get_virt_to_real(&self, virt_id: usize) -> Option<usize> {
-        self.real_to_virt.get(&virt_id).copied()
+        self.virt_to_real.get(&virt_id).copied()
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn init() {
+        // Given: An empty id map.
+        let id_map = IdMap::init();
+
+        // When: Doing nothing.
+
+        // Then:
+        assert!(id_map.virt_to_real.is_empty());
+        assert!(id_map.real_to_virt.is_empty());
+    }
+
+    #[test]
+    fn put_real_to_virt() {
+        // Given: An empty id map.
+        let mut id_map = IdMap::init();
+
+        // When: Adding a mapping from 1(real) to 2(virtual)
+        id_map.put_real_to_virt(1, 2);
+
+        // Then:
+        assert_eq!(*id_map.real_to_virt.get(&1).unwrap(), 2);
+        assert!(id_map.virt_to_real.is_empty());
+    }
+
+    #[test]
+    fn put_virt_to_real() {
+        // Given: An empty id map.
+        let mut id_map = IdMap::init();
+
+        // When: Adding a mapping from 1(virtual) to 2(real)
+        id_map.put_virt_to_real(1, 2);
+
+        // Then:
+        assert_eq!(*id_map.virt_to_real.get(&1).unwrap(), 2);
+        assert!(id_map.real_to_virt.is_empty());
+    }
+
+    #[test]
+    fn get_real_to_virt() {
+        // Given: An empty id map.
+        let mut id_map = IdMap::init();
+
+        // When: Adding a mapping from 1(real) to 2(virtual).
+        id_map.put_real_to_virt(1, 2);
+
+        // Then: Api must return the mapping.
+        assert_eq!(id_map.get_real_to_virt(1).unwrap(), 2);
+    }
+
+    #[test]
+    fn get_virt_to_real() {
+        // Given: An empty id map.
+        let mut id_map = IdMap::init();
+
+        // When: Adding a mapping between from 1(virtual) to 2(real).
+        id_map.put_virt_to_real(1, 2);
+
+        // Then: Api must return the mapping.
+        assert_eq!(id_map.get_virt_to_real(1).unwrap(), 2);
     }
 }
