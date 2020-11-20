f9627caccb23ab15dd67db2cf18cc9e05e99428c
m.amin.rayej <m.amin.rayej@gmail.com>
2020-11-20 13:10:22 +0330

    add tests for topological sort

--- src/algo/topological_sort.rs
+++ src/algo/topological_sort.rs
@@ -1,6 +1,6 @@
+use crate::algo::{Dfs, DfsListener};
 use crate::graph::{DirectedEdge, Edge};
 use crate::provide;
-use crate::algo::{Dfs, DfsListener};
 
 pub struct TopologicalSort {
     sorted_vertex_ids: Vec<usize>,
@@ -46,17 +46,67 @@ mod tests {
     use crate::storage::DiMat;
 
     #[test]
-    fn topological_sort_test() {
-        // a  -->  b.
+    fn empty_graph() {
+        let graph = MatGraph::init(DiMat::<usize>::init());
+
+        let sorted_vertices = TopologicalSort::init().execute(&graph);
+
+        assert_eq!(sorted_vertices.len(), 0);
+    }
+
+    #[test]
+    fn one_vertex_graph() {
+        let mut graph = MatGraph::init(DiMat::<usize>::init());
+        let _ = graph.add_vertex();
+
+        let sorted_vertices = TopologicalSort::init().execute(&graph);
+
+        assert_eq!(sorted_vertices.len(), 1);
+    }
+
+    #[test]
+    fn trivial_graph() {
+        // Given: Graph
+        //
+        //      a  -->  b  -->  c  -->  f
+        //      |        \      |
+        //      |         '-----|
+        //      |               |
+        //      |               v
+        //      '-----> d  -->  e
         let mut graph = MatGraph::init(DiMat::<usize>::init());
         let a = graph.add_vertex();
         let b = graph.add_vertex();
         let c = graph.add_vertex();
+        let d = graph.add_vertex();
+        let e = graph.add_vertex();
+        let f = graph.add_vertex();
+
         graph.add_edge((a, b, 1).into());
-        graph.add_edge((a, c, 1).into());
+        graph.add_edge((a, d, 1).into());
+        graph.add_edge((b, c, 1).into());
+        graph.add_edge((b, e, 1).into());
+        graph.add_edge((d, e, 1).into());
+        graph.add_edge((c, e, 1).into());
+        graph.add_edge((c, f, 1).into());
 
-        let sorted_ids = TopologicalSort::init().execute(&graph);
+        let sorted_vertices = TopologicalSort::init().execute(&graph);
 
-        println!("{:?}", sorted_ids);
+        assert_eq!(sorted_vertices.len(), 6);
+        for edge in graph.edges() {
+            let (src_id, dst_id) = (edge.get_src_id(), edge.get_dst_id());
+
+            // src must appear before dst in topological sort
+            let src_index = sorted_vertices
+                .iter()
+                .position(|v_id| *v_id == src_id)
+                .unwrap();
+            let dst_index = sorted_vertices
+                .iter()
+                .position(|v_id| *v_id == dst_id)
+                .unwrap();
+
+            assert!(src_index < dst_index)
+        }
     }
 }
