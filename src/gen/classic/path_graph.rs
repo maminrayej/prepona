b8087f57580069f2324aff1824218ac6a1d08d39
M.Amin Rayej <m.amin.rayej@gmail.com>
2021-12-06 18:24:38 +0330

    provide property test for path graph generator

--- src/gen/classic/path_graph.rs
+++ src/gen/classic/path_graph.rs
@@ -4,12 +4,16 @@ use crate::provide::{InitializableStorage, MutStorage};
 
 use crate::gen::Generator;
 
+#[derive(Debug)]
 pub struct PathGraphGenerator {
     vertex_count: usize,
 }
 
 impl PathGraphGenerator {
     pub fn init(vertex_count: usize) -> Self {
+        if vertex_count < 2 {
+            panic!("Vertex count must at least be 2 to generate a path graph")
+        }
         PathGraphGenerator { vertex_count }
     }
 }
@@ -34,5 +38,63 @@ where
         }
 
         storage
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::PathGraphGenerator;
+    use quickcheck::Arbitrary;
+
+    impl Clone for PathGraphGenerator {
+        fn clone(&self) -> Self {
+            Self {
+                vertex_count: self.vertex_count.clone(),
+            }
+        }
+    }
+
+    impl Arbitrary for PathGraphGenerator {
+        fn arbitrary(g: &mut quickcheck::Gen) -> Self {
+            let vertex_count = usize::arbitrary(g) % 16 + 2;
+
+            PathGraphGenerator::init(vertex_count)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use quickcheck_macros::quickcheck;
+
+    use crate::{
+        gen::Generator,
+        provide::{Edges, Vertices},
+        storage::AdjMap,
+    };
+
+    use super::PathGraphGenerator;
+
+    #[quickcheck]
+    fn prop_gen_path_graph(generator: PathGraphGenerator) {
+        let graph: AdjMap<(), (), false> = generator.generate();
+
+        assert_eq!(
+            graph
+                .vertex_tokens()
+                .map(|vt| graph.outgoing_edges(vt).count())
+                .filter(|out_degree| *out_degree == 1)
+                .count(),
+            2
+        );
+
+        assert_eq!(
+            graph
+                .vertex_tokens()
+                .map(|vt| graph.outgoing_edges(vt).count())
+                .filter(|out_degree| *out_degree == 2)
+                .count(),
+            graph.vertex_count() - 2
+        );
     }
 }
