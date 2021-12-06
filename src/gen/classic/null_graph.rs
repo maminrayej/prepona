369aa0796dbb4ca1bd1a9c4feba484deb8c7703d
M.Amin Rayej <m.amin.rayej@gmail.com>
2021-12-06 18:24:32 +0330

    provide property test for null graph generator

--- src/gen/classic/null_graph.rs
+++ src/gen/classic/null_graph.rs
@@ -4,6 +4,7 @@ use crate::provide::{InitializableStorage, MutStorage};
 
 use crate::gen::Generator;
 
+#[derive(Debug)]
 pub struct NullGraphGenerator;
 
 impl<S> Generator<S> for NullGraphGenerator
@@ -14,5 +15,44 @@ where
 {
     fn generate(&self) -> S {
         S::init()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::NullGraphGenerator;
+    use quickcheck::Arbitrary;
+
+    impl Clone for NullGraphGenerator {
+        fn clone(&self) -> Self {
+            Self {}
+        }
+    }
+
+    impl Arbitrary for NullGraphGenerator {
+        fn arbitrary(_: &mut quickcheck::Gen) -> Self {
+            NullGraphGenerator
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use quickcheck_macros::quickcheck;
+
+    use crate::{
+        gen::Generator,
+        provide::{Edges, Vertices},
+        storage::AdjMap,
+    };
+
+    use super::NullGraphGenerator;
+
+    #[quickcheck]
+    fn prop_gen_null_graph(generator: NullGraphGenerator) {
+        let graph: AdjMap<(), (), false> = generator.generate();
+
+        assert_eq!(graph.vertex_count(), 0);
+        assert_eq!(graph.edge_count(), 0);
     }
 }
