8646af70af057fc826cd2b9d11214a1c7fc7e9f5
M.Amin Rayej <m.amin.rayej@gmail.com>
2021-12-06 18:24:52 +0330

    provide property test for wheel graph generator

--- src/gen/classic/wheel_graph.rs
+++ src/gen/classic/wheel_graph.rs
@@ -5,6 +5,7 @@ use crate::provide::{InitializableStorage, MutStorage};
 use super::CycleGraphGenerator;
 use crate::gen::Generator;
 
+#[derive(Debug)]
 pub struct WheelGraphGenerator {
     vertex_count: usize,
 }
@@ -37,5 +38,78 @@ where
         }
 
         storage
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::WheelGraphGenerator;
+    use quickcheck::Arbitrary;
+
+    impl Clone for WheelGraphGenerator {
+        fn clone(&self) -> Self {
+            Self {
+                vertex_count: self.vertex_count.clone(),
+            }
+        }
+    }
+
+    impl Arbitrary for WheelGraphGenerator {
+        fn arbitrary(g: &mut quickcheck::Gen) -> Self {
+            let vertex_count = usize::arbitrary(g) % 16 + 4;
+
+            WheelGraphGenerator::init(vertex_count)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use itertools::Itertools;
+    use quickcheck_macros::quickcheck;
+
+    use crate::{
+        gen::Generator,
+        provide::{Edges, MutVertices, Vertices},
+        storage::AdjMap,
+    };
+
+    use super::WheelGraphGenerator;
+
+    #[quickcheck]
+    fn prop_gen_wheel_graph(generator: WheelGraphGenerator) {
+        let mut graph: AdjMap<(), (), false> = generator.generate();
+
+        let vt_out_degree: Vec<(usize, usize)> = graph
+            .vertex_tokens()
+            .map(|vt| (vt, graph.outgoing_edges(vt).count()))
+            .collect();
+
+        let universal_vt_index = vt_out_degree
+            .iter()
+            .position_max_by(|(_, deg1), (_, deg2)| deg1.cmp(deg2))
+            .unwrap();
+
+        let (universal_vt, universal_vt_deg) = vt_out_degree[universal_vt_index];
+
+        assert_eq!(universal_vt_deg, graph.vertex_count() - 1);
+
+        assert!(graph.has_vt(universal_vt));
+
+        // If we remove the universal vertex, we have a cycle
+        graph.remove_vertex(universal_vt);
+
+        for vt in graph.vertex_tokens() {
+            assert_eq!(graph.neighbors(vt).filter(|n_vt| *n_vt != vt).count(), 2);
+        }
+
+        assert_eq!(
+            graph
+                .vertex_tokens()
+                .map(|vt| graph.outgoing_edges(vt).count())
+                .filter(|out_degree| *out_degree == 2)
+                .count(),
+            graph.vertex_count()
+        );
     }
 }
