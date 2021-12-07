0bb964fad96e0eb60e5d2b1fb60effbcb6d8472b
M.Amin Rayej <m.amin.rayej@gmail.com>
2021-12-07 23:36:48 +0330

    provide property test for turan graph generator

--- src/gen/classic/turan_graph.rs
+++ src/gen/classic/turan_graph.rs
@@ -6,6 +6,7 @@ use crate::provide::{InitializableStorage, MutStorage};
 use super::CompleteMultiPartiteGraphGenerator;
 use crate::gen::Generator;
 
+#[derive(Debug)]
 pub struct TuranGraphGenerator {
     vertex_count: usize,
     partition_count: usize,
@@ -13,14 +14,26 @@ pub struct TuranGraphGenerator {
 
 impl TuranGraphGenerator {
     pub fn init(vertex_count: usize, partition_count: usize) -> Self {
-        if partition_count < 1 || partition_count > vertex_count {
-            panic!("Partition count must be between 1 and vertex count: 1 <= partition count <= vertex count")
-        }
-
         TuranGraphGenerator {
             vertex_count,
             partition_count,
         }
+    }
+
+    pub fn set_sizes_of(vertex_count: usize, partition_count: usize) -> Vec<usize> {
+        if partition_count < 1 || partition_count > vertex_count {
+            panic!("Partition size must be between 1 and vertex count: 1 <= partition size <= vertex count")
+        }
+
+        repeat_n(
+            vertex_count / partition_count,
+            partition_count - (vertex_count % partition_count),
+        )
+        .chain(repeat_n(
+            vertex_count / partition_count + 1,
+            vertex_count % partition_count,
+        ))
+        .collect()
     }
 }
 
@@ -31,15 +44,69 @@ where
     Standard: Distribution<S::E>,
 {
     fn generate(&self) -> S {
-        let set_iter = repeat_n(
-            self.vertex_count / self.partition_count,
-            self.partition_count - (self.vertex_count % self.partition_count),
-        )
-        .chain(repeat_n(
-            self.vertex_count / self.partition_count + 1,
-            self.vertex_count % self.partition_count,
-        ));
+        let set_sizes =
+            TuranGraphGenerator::set_sizes_of(self.vertex_count, self.partition_count).into_iter();
 
-        CompleteMultiPartiteGraphGenerator::init(set_iter).generate()
+        CompleteMultiPartiteGraphGenerator::init(set_sizes.into_iter()).generate()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::TuranGraphGenerator;
+    use quickcheck::Arbitrary;
+
+    impl Clone for TuranGraphGenerator {
+        fn clone(&self) -> Self {
+            Self {
+                vertex_count: self.vertex_count.clone(),
+                partition_count: self.partition_count.clone(),
+            }
+        }
+    }
+
+    impl Arbitrary for TuranGraphGenerator {
+        fn arbitrary(g: &mut quickcheck::Gen) -> Self {
+            let vertex_count = usize::arbitrary(g) % 16 + 1;
+            let partition_count = usize::arbitrary(g) % vertex_count + 1;
+
+            TuranGraphGenerator::init(vertex_count, partition_count)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use quickcheck_macros::quickcheck;
+
+    use crate::{
+        gen::Generator,
+        provide::{Edges, Vertices},
+        storage::AdjMap,
+    };
+
+    use super::TuranGraphGenerator;
+
+    #[quickcheck]
+    fn prop_gen_turan_graph(generator: TuranGraphGenerator) {
+        let graph: AdjMap<(), (), false> = generator.generate();
+
+        let vertex_count = generator.vertex_count;
+        let partition_count = generator.partition_count;
+
+        let set_sizes = TuranGraphGenerator::set_sizes_of(vertex_count, partition_count);
+
+        assert_eq!(set_sizes.iter().copied().sum::<usize>(), vertex_count);
+
+        for set_size in &set_sizes {
+            assert_eq!(
+                graph
+                    .vertex_tokens()
+                    .map(|vt| graph.outgoing_edges(vt).count())
+                    .filter(|out_degree| *out_degree == vertex_count - *set_size)
+                    .count(),
+                set_sizes.iter().filter(|ss| *ss == set_size).sum::<usize>()
+            );
+        }
     }
 }
