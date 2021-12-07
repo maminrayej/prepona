8932874f0a8dcbc4d13c48dc7c58347932e60f39
M.Amin Rayej <m.amin.rayej@gmail.com>
2021-12-07 23:36:02 +0330

    provide property test for barbell graph generator

--- src/gen/classic/barbell_graph.rs
+++ src/gen/classic/barbell_graph.rs
@@ -1,3 +1,5 @@
+use std::fmt::Debug;
+
 use itertools::Itertools;
 use rand::{distributions::Standard, prelude::Distribution, thread_rng, Rng};
 
@@ -6,22 +8,20 @@ use crate::provide::{InitializableStorage, MutStorage};
 use super::CompleteGraphGenerator;
 use crate::gen::Generator;
 
+#[derive(Debug)]
 pub struct BarbellGraphGenerator {
-    first_complete_graph_size: usize,
-    second_complete_graph_size: usize,
+    complete_graph_size: usize,
     bridge_size: usize,
 }
 
 impl BarbellGraphGenerator {
-    pub fn init(
-        first_complete_graph_size: usize,
-        second_complete_graph_size: usize,
-        bridge_size: usize,
-    ) -> Self {
-        // TODO: Validate sizes
+    pub fn init(complete_graph_size: usize, bridge_size: usize) -> Self {
+        if complete_graph_size < 3 {
+            panic!("Complete graph size must be greater than 2 to generate a barbell graph")
+        }
+
         BarbellGraphGenerator {
-            first_complete_graph_size,
-            second_complete_graph_size,
+            complete_graph_size,
             bridge_size,
         }
     }
@@ -29,20 +29,20 @@ impl BarbellGraphGenerator {
 
 impl<S> Generator<S> for BarbellGraphGenerator
 where
-    S: InitializableStorage + MutStorage,
+    S: InitializableStorage + MutStorage + Debug,
     Standard: Distribution<S::V>,
     Standard: Distribution<S::E>,
 {
     fn generate(&self) -> S {
         // Generate first compelete graph
-        let mut storage: S =
-            CompleteGraphGenerator::init(self.first_complete_graph_size).generate();
+        let mut storage: S = CompleteGraphGenerator::init(self.complete_graph_size).generate();
+
         let first_graph_tokens: Vec<usize> = storage.vertex_tokens().collect();
 
         let mut rng = thread_rng();
 
         // Create second compelete graph
-        let second_graph_tokens: Vec<usize> = (0..self.second_complete_graph_size)
+        let second_graph_tokens: Vec<usize> = (0..self.complete_graph_size)
             .into_iter()
             .map(|_| storage.add_vertex(rng.gen()))
             .collect();
@@ -53,7 +53,9 @@ where
             .cartesian_product(second_graph_tokens.iter().copied());
 
         for (src_vt, dst_vt) in vt_pairs {
-            storage.add_edge(src_vt, dst_vt, rng.gen());
+            if src_vt < dst_vt {
+                storage.add_edge(src_vt, dst_vt, rng.gen());
+            }
         }
 
         if self.bridge_size == 0 {
@@ -82,5 +84,83 @@ where
         }
 
         storage
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::BarbellGraphGenerator;
+    use quickcheck::Arbitrary;
+
+    impl Clone for BarbellGraphGenerator {
+        fn clone(&self) -> Self {
+            Self {
+                complete_graph_size: self.complete_graph_size.clone(),
+                bridge_size: self.bridge_size.clone(),
+            }
+        }
+    }
+
+    impl Arbitrary for BarbellGraphGenerator {
+        fn arbitrary(g: &mut quickcheck::Gen) -> Self {
+            let complete_graph_size = usize::arbitrary(g) % 8 + 3;
+            let bridge_size = usize::arbitrary(g) % 8;
+
+            BarbellGraphGenerator::init(complete_graph_size, bridge_size)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use quickcheck_macros::quickcheck;
+
+    use crate::{
+        gen::Generator,
+        provide::{Edges, Vertices},
+        storage::AdjMap,
+    };
+
+    use super::BarbellGraphGenerator;
+
+    #[quickcheck]
+    fn prop_gen_barbell_graph(generator: BarbellGraphGenerator) {
+        let graph: AdjMap<(), (), false> = generator.generate();
+
+        assert_eq!(
+            graph
+                .vertex_tokens()
+                .map(|vt| graph.outgoing_edges(vt).count())
+                .filter(|out_degree| *out_degree == generator.complete_graph_size - 1)
+                .count(),
+            generator.complete_graph_size * 2 - 2
+                + if generator.complete_graph_size - 1 == 2 {
+                    generator.bridge_size
+                } else {
+                    0
+                }
+        );
+        assert_eq!(
+            graph
+                .vertex_tokens()
+                .map(|vt| graph.outgoing_edges(vt).count())
+                .filter(|out_degree| *out_degree == generator.complete_graph_size)
+                .count(),
+            2
+        );
+
+        assert_eq!(
+            graph
+                .vertex_tokens()
+                .map(|vt| graph.outgoing_edges(vt).count())
+                .filter(|out_degree| *out_degree == 2)
+                .count(),
+            generator.bridge_size
+                + if generator.complete_graph_size - 1 == 2 {
+                    generator.complete_graph_size * 2 - 2
+                } else {
+                    0
+                }
+        );
     }
 }
