d350b6145c4e3865c9aaedb8d98202aa2701915e
M.Amin Rayej <m.amin.rayej@protonmail.com>
2022-01-25 08:38:36 +0330

    add property tests for SimpleGraph

--- src/graph/simple_graph.rs
+++ src/graph/simple_graph.rs
@@ -1,5 +1,6 @@
 use std::marker::PhantomData;
 
+use crate::storage::AdjMap;
 use crate::{
     provide::{
         CheckedEdges, CheckedMutEdges, CheckedVertices, Edges, InitializableStorage, MutEdges,
@@ -13,6 +14,10 @@ use crate::{
 
 use super::GraphError;
 
+pub type SimpleGraphMap<V, E, DIR> = SimpleGraph<AdjMap<V, E, DIR>, V, E, DIR>;
+
+// TODO: Add Direction trait bound to S
+#[derive(Debug)]
 pub struct SimpleGraph<S, V, E, Dir>
 where
     V: VertexDescriptor,
@@ -41,6 +46,22 @@ where
             phantom_e: PhantomData,
             phantom_dir: PhantomData,
         }
+    }
+}
+
+impl<S, V, E, Dir> Direction for SimpleGraph<S, V, E, Dir>
+where
+    V: VertexDescriptor,
+    E: EdgeDescriptor,
+    Dir: Direction,
+    S: InitializableStorage<Dir = Dir> + Vertices<V = V, Dir = Dir> + Edges<E = E, Dir = Dir>,
+{
+    fn is_directed() -> bool {
+        Dir::is_directed()
+    }
+
+    fn is_undirected() -> bool {
+        Dir::is_undirected()
     }
 }
 
@@ -229,5 +250,215 @@ where
         }
 
         self.storage.add_edge_checked(src_vt, dst_vt, edge)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::collections::HashMap;
+    use std::marker::PhantomData;
+
+    use quickcheck::Arbitrary;
+    use rand::{thread_rng, Rng};
+
+    use crate::provide::{Edges, InitializableStorage, MutEdges, MutVertices, Vertices};
+    use crate::storage::edge::{Direction, EdgeDescriptor};
+    use crate::storage::vertex::VertexDescriptor;
+
+    use super::SimpleGraph;
+
+    impl<S, V, E, Dir> Clone for SimpleGraph<S, V, E, Dir>
+    where
+        V: VertexDescriptor,
+        E: EdgeDescriptor,
+        Dir: Direction,
+        S: InitializableStorage<Dir = Dir>
+            + Vertices<V = V, Dir = Dir>
+            + Edges<E = E, Dir = Dir>
+            + Clone,
+    {
+        fn clone(&self) -> Self {
+            Self {
+                storage: self.storage.clone(),
+                phantom_v: self.phantom_v.clone(),
+                phantom_e: self.phantom_e.clone(),
+                phantom_dir: self.phantom_dir.clone(),
+            }
+        }
+    }
+
+    impl<S, V, E, Dir> Arbitrary for SimpleGraph<S, V, E, Dir>
+    where
+        V: VertexDescriptor + Arbitrary,
+        E: EdgeDescriptor + Arbitrary,
+        Dir: Direction + Arbitrary,
+        S: InitializableStorage<Dir = Dir>
+            + Vertices<V = V, Dir = Dir>
+            + Edges<E = E, Dir = Dir>
+            + MutVertices
+            + MutEdges
+            + Arbitrary,
+    {
+        fn arbitrary(g: &mut quickcheck::Gen) -> Self {
+            let vertex_count = usize::arbitrary(g) % 100;
+
+            let mut rng = thread_rng();
+            let edge_probability = rng.gen::<f64>() * rng.gen::<f64>();
+
+            let mut adj_map = S::init();
+
+            let vts: Vec<usize> = (0..vertex_count)
+                .map(|_| adj_map.add_vertex(V::arbitrary(g)))
+                .collect();
+
+            vts.iter().zip(vts.iter()).for_each(|(i, j)| {
+                let p = rng.gen::<f64>();
+
+                if i < j && p <= edge_probability {
+                    let num_of_edges = (usize::arbitrary(g) % 3) + 1;
+
+                    for _ in 0..num_of_edges {
+                        adj_map.add_edge(*i, *j, E::arbitrary(g));
+                    }
+                }
+            });
+
+            SimpleGraph {
+                storage: adj_map,
+                phantom_v: PhantomData,
+                phantom_e: PhantomData,
+                phantom_dir: PhantomData,
+            }
+        }
+
+        fn shrink(&self) -> Box<dyn Iterator<Item = Self>> {
+            let mut graph_odd = Self::init();
+            let mut graph_even = Self::init();
+
+            let mut vt_map = HashMap::new();
+
+            for (index, vt) in self.vertex_tokens().enumerate() {
+                let new_vt = if index % 2 == 0 {
+                    graph_odd.add_vertex(self.vertex(vt).clone())
+                } else {
+                    graph_even.add_vertex(self.vertex(vt).clone())
+                };
+
+                vt_map.insert(vt, new_vt);
+            }
+
+            for (src_vt, dst_vt, edge) in self.edges() {
+                let new_src_vt = vt_map[&src_vt];
+                let new_dst_vt = vt_map[&dst_vt];
+
+                if graph_odd.has_vt(new_src_vt) && graph_odd.has_vt(new_dst_vt) {
+                    graph_odd.add_edge(new_src_vt, new_dst_vt, edge.clone());
+                } else if graph_even.has_vt(new_src_vt) && graph_even.has_vt(new_dst_vt) {
+                    graph_even.add_edge(new_src_vt, new_dst_vt, edge.clone());
+                }
+            }
+
+            let before_count = self.vertex_count();
+
+            Box::new(
+                [graph_odd, graph_even]
+                    .into_iter()
+                    .filter(move |graph| graph.vertex_count() < before_count),
+            )
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use itertools::Itertools;
+    use rand::prelude::IteratorRandom;
+    use rand::thread_rng;
+
+    use crate::graph::SimpleGraphMap;
+    use crate::provide::{CheckedMutEdges, MutEdges, Vertices};
+    use crate::storage::edge::{Directed, Direction, Undirected};
+    use crate::test_utils::get_non_duplicate;
+
+    #[should_panic]
+    #[test]
+    fn prop_add_loop() {
+        fn prop<Dir: Direction>(mut graph: SimpleGraphMap<(), (), Dir>) {
+            if graph.vertex_count() == 0 {
+                return;
+            }
+
+            let vertex_id = graph.vertex_tokens().choose(&mut thread_rng()).unwrap();
+
+            graph.add_edge(vertex_id, vertex_id, ());
+        }
+
+        quickcheck::quickcheck(prop as fn(SimpleGraphMap<(), (), Undirected>));
+        quickcheck::quickcheck(prop as fn(SimpleGraphMap<(), (), Directed>));
+    }
+
+    #[test]
+    fn prop_add_loop_checked() {
+        fn prop<Dir: Direction>(mut graph: SimpleGraphMap<(), (), Dir>) -> bool {
+            if graph.vertex_count() == 0 {
+                return true;
+            }
+
+            let vertex_id = graph.vertex_tokens().choose(&mut thread_rng()).unwrap();
+
+            graph.add_edge_checked(vertex_id, vertex_id, ()).is_err()
+        }
+
+        quickcheck::quickcheck(prop as fn(SimpleGraphMap<(), (), Undirected>) -> bool);
+        quickcheck::quickcheck(prop as fn(SimpleGraphMap<(), (), Directed>) -> bool);
+    }
+
+    #[should_panic]
+    #[test]
+    fn prop_add_multi_edge() {
+        fn prop<Dir: Direction>(mut graph: SimpleGraphMap<(), (), Dir>) {
+            if graph.vertex_count() < 2 {
+                return;
+            }
+
+            let vertex_ids = get_non_duplicate(graph.vertex_tokens(), 2);
+
+            let src_vid = vertex_ids[0];
+            let dst_vid = vertex_ids[1];
+
+            if graph.neighbors(src_vid).contains(&dst_vid) {
+                graph.add_edge(src_vid, dst_vid, ());
+            } else {
+                graph.add_edge(src_vid, dst_vid, ());
+                graph.add_edge(src_vid, dst_vid, ());
+            }
+        }
+
+        quickcheck::quickcheck(prop as fn(SimpleGraphMap<(), (), Undirected>));
+        quickcheck::quickcheck(prop as fn(SimpleGraphMap<(), (), Directed>));
+    }
+
+    #[test]
+    fn prop_add_multi_edge_checked() {
+        fn prop<Dir: Direction>(mut graph: SimpleGraphMap<(), (), Dir>) -> bool {
+            if graph.vertex_count() < 2 {
+                return true;
+            }
+
+            let vertex_ids = graph.vertex_tokens().choose_multiple(&mut thread_rng(), 2);
+
+            let src_vid = vertex_ids[0];
+            let dst_vid = vertex_ids[1];
+
+            if graph.neighbors(src_vid).contains(&dst_vid) {
+                graph.add_edge_checked(src_vid, dst_vid, ()).is_err()
+            } else {
+                graph.add_edge_checked(src_vid, dst_vid, ()).is_ok()
+                    && graph.add_edge_checked(src_vid, dst_vid, ()).is_err()
+            }
+        }
+
+        quickcheck::quickcheck(prop as fn(SimpleGraphMap<(), (), Undirected>) -> bool);
+        quickcheck::quickcheck(prop as fn(SimpleGraphMap<(), (), Directed>) -> bool);
     }
 }
