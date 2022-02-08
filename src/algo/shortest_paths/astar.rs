fb56e9ab9c0b929d2631a2eb6d0df2b3ea5a1441
M.Amin Rayej <m.amin.rayej@protonmail.com>
2022-02-08 21:09:29 +0330

    implement astar algorithm

---
+++ src/algo/shortest_paths/astar.rs
@@ -0,0 +1,119 @@
+use std::collections::{BinaryHeap, HashMap, HashSet};
+
+use magnitude::Magnitude;
+
+use crate::common::{RealID, VirtID};
+use crate::provide::{Edges, Storage, Vertices};
+use crate::storage::edge::Directed;
+use crate::view::GenericView;
+
+#[derive(PartialEq, Eq)]
+struct VertexScoreHue(RealID, Magnitude<usize>, Magnitude<usize>);
+
+impl PartialOrd for VertexScoreHue {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.2.partial_cmp(&other.2)
+    }
+}
+
+impl Ord for VertexScoreHue {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.partial_cmp(other).unwrap()
+    }
+}
+
+pub fn astar<G, F, H>(
+    graph: &G,
+    src_rid: RealID,
+    target_id: RealID,
+    cost_of: F,
+    estimate_of: H,
+) -> (GenericView<'_, G>, HashMap<usize, usize>)
+where
+    G: Storage<Dir = Directed> + Vertices + Edges,
+    F: Fn(usize) -> usize,
+    H: Fn(usize) -> usize,
+{
+    let vertex_count = graph.vertex_count();
+    let id_map = graph.id_map();
+    let src_vid = id_map[src_rid];
+
+    let mut visited = vec![false; vertex_count];
+    let mut costs = vec![Magnitude::PosInfinite; vertex_count];
+    let mut visited_edges = HashSet::new();
+    let mut heap = BinaryHeap::new();
+
+    visited[src_vid.inner()] = true;
+    costs[src_vid.inner()] = 0.into();
+    heap.push(VertexScoreHue(
+        src_rid,
+        0.into(),
+        estimate_of(src_rid.inner()).into(),
+    ));
+
+    while let Some(VertexScoreHue(u_rid, u_cost, _)) = heap.pop() {
+        let u_vid = id_map[u_rid];
+
+        if visited[u_vid.inner()] {
+            continue;
+        }
+
+        if u_rid == target_id {
+            visited[u_vid.inner()] = true;
+            break;
+        }
+
+        for eid in graph.outgoing_edges(u_rid.inner()) {
+            let (_, v_id, _) = graph.edge(eid);
+            let v_rid = RealID::from(v_id);
+            let v_vid = id_map[v_rid];
+
+            if visited[v_vid.inner()] {
+                continue;
+            }
+
+            let cost = u_cost + cost_of(eid).into();
+            let current_cost = costs[v_vid.inner()];
+
+            if cost < current_cost {
+                costs[v_vid.inner()] = cost;
+                // FIXME: storing eid is not corrent. because parent of v can change. it may be
+                // wrong in dijkstra too.
+                visited_edges.insert(eid);
+                heap.push(VertexScoreHue(
+                    v_rid,
+                    cost,
+                    cost + estimate_of(v_rid.inner()).into(),
+                ));
+            }
+        }
+
+        visited[u_vid.inner()] = true;
+    }
+
+    let tree_view = GenericView::init(
+        graph,
+        |vid| {
+            let v_vid = id_map[RealID::from(vid)];
+            visited[v_vid.inner()]
+        },
+        |eid| visited_edges.contains(&eid),
+    );
+
+    let cost_map = costs
+        .into_iter()
+        .enumerate()
+        .filter_map(|(index, cost)| {
+            let v_vid = VirtID::from(index);
+            let v_rid = id_map[v_vid];
+
+            if visited[index] {
+                Some((v_rid.inner(), cost.unwrap()))
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    (tree_view, cost_map)
+}
--- src/algo/shortest_paths/mod.rs
+++ src/algo/shortest_paths/mod.rs
@@ -1,7 +1,9 @@
 mod bellman_ford;
 mod dijkstra;
 mod floyd_warshall;
+mod astar;
 
 pub use bellman_ford::*;
 pub use dijkstra::*;
 pub use floyd_warshall::*;
+pub use astar::*;
