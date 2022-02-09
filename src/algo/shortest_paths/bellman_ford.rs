526a3b8210a08c20e094dbddd46974cd416e6e13
M. Amin Rayej <m.amin.rayej@protonmail.com>
2022-02-10 02:58:32 +0330

    provide property tests for bellman ford algorithm

--- src/algo/shortest_paths/bellman_ford.rs
+++ src/algo/shortest_paths/bellman_ford.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 
 use anyhow::Result;
 use magnitude::Magnitude;
@@ -11,6 +11,7 @@ use crate::view::GenericView;
 
 pub fn bellman_ford<G, F>(
     graph: &G,
+    src_rid: RealID,
     cost_of: F,
 ) -> Result<(GenericView<'_, G>, HashMap<usize, isize>)>
 where
@@ -19,9 +20,12 @@ where
 {
     let vertex_count = graph.vertex_count();
     let id_map = graph.id_map();
+    let src_vid = id_map[src_rid];
 
     let mut costs = vec![Magnitude::PosInfinite; vertex_count];
-    let mut visited_edges = HashSet::new();
+    let mut used_edges = vec![None; vertex_count];
+
+    costs[src_vid.inner()] = 0.into();
 
     for _ in 1..vertex_count {
         for eid in graph.edge_tokens() {
@@ -34,9 +38,9 @@ where
             let d_cost = costs[d_vid.inner()];
 
             let new_cost = s_cost + cost_of(eid).into();
-            if new_cost.is_finite() && d_cost > new_cost {
+            if new_cost.is_finite() && new_cost < d_cost {
                 costs[d_vid.inner()] = new_cost;
-                visited_edges.insert(eid);
+                used_edges[d_vid.inner()] = Some(eid);
             }
         }
     }
@@ -51,7 +55,7 @@ where
         let d_cost = costs[d_vid.inner()];
 
         let new_cost = s_cost + cost_of(eid).into();
-        if new_cost.is_finite() && d_cost > new_cost {
+        if new_cost.is_finite() && new_cost < d_cost {
             return Err(
                 AlgoError::InvalidArgument("Graph contains negative cycle".to_string()).into(),
             );
@@ -64,7 +68,7 @@ where
             let v_vid = id_map[RealID::from(vid)];
             costs[v_vid.inner()].is_finite()
         },
-        |eid| visited_edges.contains(&eid),
+        |eid| used_edges.contains(&Some(eid)),
     );
 
     let cost_map = costs
@@ -83,4 +87,104 @@ where
         .collect();
 
     Ok((tree_view, cost_map))
+}
+
+#[cfg(test)]
+mod tests {
+    use itertools::Itertools;
+    use quickcheck_macros::quickcheck;
+
+    use crate::algo::shortest_paths::bellman_ford;
+    use crate::common::RealID;
+    use crate::gen::{
+        CompleteGraphGenerator, CycleGraphGenerator, EmptyGraphGenerator, Generator,
+        PathGraphGenerator,
+    };
+    use crate::provide::{Edges, Vertices};
+    use crate::storage::edge::Directed;
+    use crate::storage::AdjMap;
+    #[quickcheck]
+    fn prop_bellman_ford_on_empty_graph(generator: EmptyGraphGenerator) {
+        let graph: AdjMap<(), (), Directed> = generator.generate();
+
+        if graph.vertex_count() > 0 {
+            let (tree_view, costs) = bellman_ford(&graph, RealID::from(0), |_| 1).unwrap();
+
+            assert_eq!(tree_view.vertex_count(), 1);
+            assert_eq!(tree_view.edge_count(), 0);
+            assert_eq!(costs.len(), 1);
+            assert_eq!(costs[&0], 0);
+        }
+    }
+
+    #[quickcheck]
+    fn prop_bellman_ford_on_complete_graph(generator: CompleteGraphGenerator) {
+        let graph: AdjMap<(), (), Directed> = generator.generate();
+
+        let (tree_view, costs) = bellman_ford(&graph, RealID::from(0), |_| 1).unwrap();
+
+        assert_eq!(tree_view.vertex_count(), graph.vertex_count());
+        assert_eq!(tree_view.edge_count(), tree_view.vertex_count() - 1);
+        tree_view.vertex_tokens().for_each(|vid| {
+            let cost = costs[&vid];
+
+            if vid == 0 {
+                assert_eq!(cost, 0);
+            } else {
+                assert_eq!(cost, 1);
+            }
+        });
+    }
+
+    #[quickcheck]
+    fn prop_bellman_ford_on_path_graph(generator: PathGraphGenerator) {
+        let graph: AdjMap<(), (), Directed> = generator.generate();
+
+        // Find the vertex with in degree of zero.
+        // Every other vertex must be accessible from this vertex.
+        let start_id = graph
+            .vertex_tokens()
+            .find(|vid| graph.ingoing_edges(*vid).count() == 0)
+            .unwrap();
+
+        let (tree_view, costs) = bellman_ford(&graph, RealID::from(start_id), |_| 1).unwrap();
+
+        assert_eq!(tree_view.vertex_count(), graph.vertex_count());
+        assert_eq!(tree_view.edge_count(), tree_view.vertex_count() - 1);
+        assert_eq!(tree_view.edge_count(), graph.edge_count());
+
+        let mut sorted_costs = costs.values().collect_vec();
+        sorted_costs.sort();
+
+        sorted_costs.iter().enumerate().for_each(|(index, cost)| {
+            assert_eq!(index as isize, **cost);
+        });
+    }
+
+    #[quickcheck]
+    fn prop_bellman_ford_on_cycle_graph(generator: CycleGraphGenerator) {
+        let graph: AdjMap<(), (), Directed> = generator.generate();
+
+        // Because the graph is circular, it doesn't matter from which vertex to start the
+        // bellman_ford.
+        let (tree_view, costs) = bellman_ford(&graph, RealID::from(0), |_| 1).unwrap();
+
+        assert_eq!(tree_view.vertex_count(), graph.vertex_count());
+        assert_eq!(tree_view.edge_count(), tree_view.vertex_count() - 1);
+        assert_eq!(tree_view.edge_count(), graph.edge_count() - 1);
+
+        let mut sorted_costs = costs.values().collect_vec();
+        sorted_costs.sort();
+
+        sorted_costs.iter().enumerate().for_each(|(index, cost)| {
+            assert_eq!(index as isize, **cost);
+        });
+    }
+
+    #[quickcheck]
+    fn prop_bellman_ford_on_graph_with_negative_cycle(generator: CycleGraphGenerator) {
+        let graph: AdjMap<(), (), Directed> = generator.generate();
+
+        assert!(bellman_ford(&graph, RealID::from(0), |_| -1).is_err())
+    }
 }
