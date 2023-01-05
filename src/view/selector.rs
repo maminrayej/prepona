c8301f82a5c3881d7d45a26884a1c17da60b8fa4
maminrayej <m.amin.rayej@gmail.com>
2023-01-05 20:36:49 +0330

    implement combinators for selectors

--- src/view/selector.rs
+++ src/view/selector.rs
@@ -3,14 +3,39 @@ pub trait Selector {
     type Element;
 
     fn select(&self, storage: &Self::Storage, element: &Self::Element) -> bool;
+
+    fn and<'a, S>(&'a self, rhs: &'a S) -> AndSelector<Self, S> {
+        AndSelector {
+            sel1: self,
+            sel2: rhs,
+        }
+    }
+
+    fn or<'a, S>(&'a self, rhs: &'a S) -> OrSelector<Self, S> {
+        OrSelector {
+            sel1: self,
+            sel2: rhs,
+        }
+    }
+
+    fn xor<'a, S>(&'a self, rhs: &'a S) -> XORSelector<Self, S> {
+        XORSelector {
+            sel1: self,
+            sel2: rhs,
+        }
+    }
+
+    fn not(&self) -> NotSelector<Self> {
+        NotSelector { sel: self }
+    }
 }
 
-pub struct AndSelector<S1, S2> {
-    selector1: S1,
-    selector2: S2,
+pub struct AndSelector<'a, S1: ?Sized, S2: ?Sized> {
+    sel1: &'a S1,
+    sel2: &'a S2,
 }
 
-impl<S1, S2> Selector for AndSelector<S1, S2>
+impl<'a, S1, S2> Selector for AndSelector<'a, S1, S2>
 where
     S1: Selector,
     S2: Selector<Storage = S1::Storage, Element = S1::Element>,
@@ -20,16 +45,16 @@ where
     type Element = S1::Element;
 
     fn select(&self, storage: &Self::Storage, element: &Self::Element) -> bool {
-        self.selector1.select(storage, element) && self.selector2.select(storage, element)
+        self.sel1.select(storage, element) && self.sel2.select(storage, element)
     }
 }
 
-pub struct OrSelector<S1, S2> {
-    selector1: S1,
-    selector2: S2,
+pub struct OrSelector<'a, S1: ?Sized, S2: ?Sized> {
+    sel1: &'a S1,
+    sel2: &'a S2,
 }
 
-impl<S1, S2> Selector for OrSelector<S1, S2>
+impl<'a, S1, S2> Selector for OrSelector<'a, S1, S2>
 where
     S1: Selector,
     S2: Selector<Storage = S1::Storage, Element = S1::Element>,
@@ -39,15 +64,15 @@ where
     type Element = S1::Element;
 
     fn select(&self, storage: &Self::Storage, element: &Self::Element) -> bool {
-        self.selector1.select(storage, element) || self.selector2.select(storage, element)
+        self.sel1.select(storage, element) || self.sel2.select(storage, element)
     }
 }
 
-pub struct NotSelector<S> {
-    selector: S,
+pub struct NotSelector<'a, S: ?Sized> {
+    sel: &'a S,
 }
 
-impl<S> Selector for NotSelector<S>
+impl<'a, S> Selector for NotSelector<'a, S>
 where
     S: Selector,
 {
@@ -56,16 +81,16 @@ where
     type Element = S::Element;
 
     fn select(&self, storage: &Self::Storage, element: &Self::Element) -> bool {
-        !self.selector.select(storage, element)
+        !self.sel.select(storage, element)
     }
 }
 
-pub struct XORSelector<S1, S2> {
-    selector1: S1,
-    selector2: S2,
+pub struct XORSelector<'a, S1: ?Sized, S2: ?Sized> {
+    sel1: &'a S1,
+    sel2: &'a S2,
 }
 
-impl<S1, S2> Selector for XORSelector<S1, S2>
+impl<'a, S1, S2> Selector for XORSelector<'a, S1, S2>
 where
     S1: Selector,
     S2: Selector<Storage = S1::Storage, Element = S1::Element>,
@@ -75,6 +100,6 @@ where
     type Element = S1::Element;
 
     fn select(&self, storage: &Self::Storage, element: &Self::Element) -> bool {
-        self.selector1.select(storage, element) ^ self.selector2.select(storage, element)
+        self.sel1.select(storage, element) ^ self.sel2.select(storage, element)
     }
 }
